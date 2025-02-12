bb48c5a49c0ac2f1cd6f3373ca528afa542acb73
Vivek Bhagat <visual.vivek@hotmail.com>
2025-02-12 19:51:16 +1100

    Added Admin UI Controller, adjustment in program cs and swagger

---
+++ MicroNpmRegistry/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MicroNpmRegistry.Controllers
+{
+    [Route("AdminUi")]
+    public class AdminController : Controller
+    {
+        [Route("Index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MicroNpmRegistry/Program.cs
+++ MicroNpmRegistry/Program.cs
@@ -1,5 +1,8 @@
 
 using MicroNpmRegistry.Entities;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
 
 namespace MicroNpmRegistry
 {
@@ -11,10 +14,18 @@ namespace MicroNpmRegistry
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllersWithViews();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                // Exclude MVC Controllers from Swagger
+                c.CustomOperationIds(apiDesc => apiDesc.ActionDescriptor is ControllerActionDescriptor controllerAction &&
+                                                 !controllerAction.ControllerTypeInfo.IsSubclassOf(typeof(Controller))
+                                                 ? apiDesc.ActionDescriptor.Id
+                                                 : null);
+            });
             builder.Services.Configure<RegistrySettings>(builder.Configuration.GetSection("RegistrySettings"));
 
             var app = builder.Build();
@@ -23,16 +34,21 @@ namespace MicroNpmRegistry
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
+                    c.RoutePrefix = "swagger"; // Swagger UI available at /swagger
+                });
             }
-
+            app.UseRouting();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
-
+           
+            app.MapControllerRoute(
+                   name: "default",
+                    pattern: "{controller=adminui}/{action=Index}/{id?}");
             app.MapControllers();
-
             app.Run();
         }
     }
